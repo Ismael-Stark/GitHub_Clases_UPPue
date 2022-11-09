59823ce7e5710f46003b2fb1276f04b94eb2ead2
PicTrance <96091148+Ismael-Stark@users.noreply.github.com>
2022-11-09 13:53:41 -0600

    x
    x

--- Logica de Programacion/2022/1B/2_tiposVariables.c
+++ Logica de Programacion/2022/1B/2_tiposVariables.c
@@ -18,8 +18,67 @@ el archivo llamar
 */
 
 
+
+/* tipos de variables
+cacter:	para guardar letras que esten en el codigo ASCII
+
+enteras: numeros enteros con signo y sin signo, no deben de llevar
+numeros decimales
+
+logicas:	verdadero/falso		true/false		1/0, no puede guardar
+otra cosa que no sea valores logicos
+
+reales:	 todos los numeros ya sea con signo y sin signo, asi como numeros
+con o sin decimales
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+cacter:	char
+
+enteras: char, int, long, short
+
+logicas: como tal no hay uno para definir algo que guarde valores logicos
+pero sepuede usar: char o int, aunque en realidad se usa bool, pero se debe de hacer
+algo extra para poder usarlo dentro de C
+
+reales:	float, double
+
+*/
+
+//unsiged y signed solo debe usarse con int(short,long) y char
 int main(){
-	
-	printf("Hola mundo\n");
-	printf("segunda \"linea\"\n\t\tEj_Tabulador");
+	//tipo_de_variable NombreVariable;
+	char letra = 'w';//comilla ' ' simple es para guardar o de clarar
+						//una variable que guarda solamente una letra
+						//" " esto es comilla doble, o simplemnte comillas, no se usa para una sola letra
+	char numero = 84;
+	unsigned int entero = 4294967295;
+	long a = 321432;
+	short s = 10000;
+	//bool logica;
+	float flotante = 3211.123;
+	double doble = 567.321;
+
+	//printf("AgunTexto %tipoVariable %tipoVariable2 %tipoVariable3\n",mi_varAimprimir1, mi_varAimprimir2, mi_varAimprimir3.... );
+	printf("Mi varible \"letra\" contiene: %c y en decimal vale: %d\n",letra,letra);
+	printf("Mi varible \"numero\" contiene: %c y en decimal vale: %d\n",numero,numero);
+	printf("entero vale: %u\n",entero);
+	printf("long a vale: %i\n",a);
+	printf("short s vale: %i\n",s);
+	printf("float vale: %f\n",flotante);
+	printf("doble vale: %f\n",doble);
+	/*formas de imprimir tipos de vaiables:
+	//https://learn.microsoft.com/es-es/cpp/c-runtime-library/format-specification-syntax-printf-and-wprintf-functions?view=msvc-170
+	//https://www.it.uc3m.es/pbasanta/asng/course_notes/input_output_printf_es.html#:~:text=Por%20ejemplo%20printf(%22%25*d,en%20blanco%20a%20la%20izquierda).&text=*-,Igual%20que%20el%20caso%20anterior%2C%20pero%20el%20n%C3%BAmero%20a%20utilizar,%22)%20imprime%20%E2%80%9Cabc%E2%80%9D.
+		%d para imprimir numeros enteros en decimal
+		%i para imprimir numeros enteros en decimal
+		%u para imprimir numeros sin signo
+		%f para imprimir float
+		%c para imprimir caracteres
+		%s para imprimir cadena de caracteres (string)
+		%ld, lf  para imprimir numeros decimales o flotantes del tipo long
+		%lu
+		%lld para un numero long long int, osea un numero int de 64bits
+		%x hexadecimal
+	*/
 }
