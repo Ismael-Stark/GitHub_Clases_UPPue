12c9f0dda942d2d74e9acd496d380f2f3fe62f39
PicTrance <96091148+Ismael-Stark@users.noreply.github.com>
2023-03-06 13:33:36 -0600

    x
    x

---
+++ Programacion_Estructurada/2023/1A/vectores.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int tomates[13];
+    int mes;
+    tomates[1] = 200;
+    tomates[3] = 100;
+    printf("\n\nla cantidad de tomates del mes: %i,es: %iKg\n\n",2, tomates[3]);
+
+}
